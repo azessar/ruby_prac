073bbc139c8928386a78ecdb47c7b18087add6c7
azessar <akzessar@gmail.com>
2020-11-23 11:07:31 -0600

    perm

---
+++ perm.rb
@@ -0,0 +1,15 @@
+#are they permutations?
+def perm?(str1, str2)
+    return false if str1.length != str2.length
+    hash1 = {}
+    hash2 = {}
+    ("a".."z").to_a.each {|char| hash1[char] = 0}
+    ("a".."z").to_a.each {|char| hash2[char] = 0}
+    str1.each_char {|char| hash1[char] += 1}
+    str2.each_char {|char| hash2[char] += 1}
+    hash1 == hash2
+end
+
+p perm?("cat", "act")
+p perm?("abcd", "dbca")
+p perm?("hhhh", "hh")
