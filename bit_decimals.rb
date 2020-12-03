1fb390a43c8c60de64bc800ed58fe5ddbcb747f0
azessar <akzessar@gmail.com>
2020-12-02 23:08:44 -0600

    binary decimal

--- bit_decimals.rb
+++ bit_decimals.rb
@@ -6,11 +6,35 @@ def to_binary(decimal)
         biggest_fac /= 2
     end
     remainder = decimal - biggest_fac
-    [biggest_fac, remainder]
+    # [clean_binary(biggest_fac), to_binary(remainder)]
+    # raw_remainder = to_binary(remainder).to_s
+    # clean_binary(biggest_fac) + remove_front(raw_remainder)
+    clean_binary(biggest_fac) + remove_front(to_binary(remainder).to_s)
+
+end
+
+def clean_binary(num)
+    binary = "1"
+    until num == 1
+        binary = "0" + binary
+        num *= 2
+    end
+    return "." + binary[1..-1]
+end
+
+def remove_front(string)
+    i = 0
+    until string[i] == "0"
+        i += 1
+    end
+    string[i+1.. -1]
 end
 
 p to_binary(0.5)
-p to_binary(0.7)
 p to_binary(0.75)
 p to_binary(0.25)
+p to_binary(0.125)
+p to_binary(0.0625)
+p to_binary(0.8125)
+
 p to_binary(-5)
